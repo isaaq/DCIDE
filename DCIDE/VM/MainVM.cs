9ecfb43bac5d9ab00741ab2a3b8852326e5f46bf
落星星 <prec@163.com>
2020-04-14 10:03:49 +0800

    表设计器控件

---
+++ DCIDE/VM/MainVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DC.IDE.UI.Model;
+using DC.IDE.UI.Util;
+using PostSharp.Patterns.Model;
+using Telerik.Windows.Controls;
+
+namespace DCIDE.VM
+{
+    [NotifyPropertyChanged]
+    public class MainVM : ViewModelBase
+    {
+        public ObservableCollection<FuncItem> FuncList { get; set; }
+
+        public MainVM()
+        {
+            FuncList = new ObservableCollection<FuncItem>();
+            var nodelist = XmlHelper.Load("");
+            FuncList.Add(new FuncItem() { Name="Test"});
+        }
+
+    }
+}
